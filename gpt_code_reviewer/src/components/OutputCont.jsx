b14fb7708ef224ad032ddb314c1a5085b63765bd
aamirfarookh <aamirfarooqbhatt@gmail.com>
2023-08-05 00:26:29 +0530

    frontend added

---
+++ gpt_code_reviewer/src/components/OutputCont.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function OutputCont({value}) {
+  return (
+    <div className='output-cont'>
+        <code>{value}</code>
+    </div>
+  )
+}
+
+export default OutputCont
